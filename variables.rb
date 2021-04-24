0e8c5e3923ddfe8dc473dc5878ac0a4c7870096d
Matt Kragen <mattdkragen@gmail.com>
2021-04-23 17:06:23 -0700

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,17 @@
+famous_person = "Albert Einstein"
+
+is_active = false
+
+famous_person = "Jason Statham"
+
+num_3 = 3
+
+num_9 = 9
+
+sum = num_3 + num_9
+
+animals = ["fish", "pony", "hippopotamus"]
+
+animals.shift
+
+famous_person.size
