f0b0b67fc2c1f929ac7d93126704dfb24b302188
Matt Kragen <mattdkragen@gmail.com>
2021-04-23 17:19:47 -0700

    Complete program specific challenge

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+school = {
+  'principal' => "Mr. Goldberg",
+  'janitor' => "Mr. Brown",
+  'bathrooms' => 12,
+  'desks' => 200,
+  'accredited' => true,
+  'clean' => false,
+  'accolades' => ["Great place!", "I'd send my kid there!", "Wow!"],
+  'cautions' => ["Bad neighborhood", "Poor success rate", "Angry teachers"]
+}
