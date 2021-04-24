12950af65f4b6918c4a312a0d20d55858468148a
Matt Kragen <mattdkragen@gmail.com>
2021-04-23 17:26:23 -0700

    Complete program specific challenge

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+pet = {
+  'name' => "Spot",
+  'owner' => "Mark",
+  'toys' => 10,
+  'bones' => 2,
+  'hungry' => true,
+  'playful' => false,
+  'homes' => ["Bad owner", "Humane Society", "Mark"],
+  'friends' => ["Fido", "Xena", "Red"]
+}
